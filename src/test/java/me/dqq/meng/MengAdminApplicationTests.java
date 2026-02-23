ac84b6a4ca3de9ae27bca87c0e08fabec70a77b2
zhoujinhua <zmissu@163.com>
2026-02-23 11:54:57 +0800

    Initial commit

---
+++ src/test/java/me/dqq/meng/MengAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dqq.meng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MengAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
